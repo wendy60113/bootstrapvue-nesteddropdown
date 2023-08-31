07628e2f7b02c17e146c9b5f9e1ba9a90ec64a34
wendy60113 <wendy60113@gmail.com>
2023-08-31 21:18:17 +0800

    edit sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git add -A
 git commit -m 'deploy'
 
 # 將 dist資料夾中的內容推送至遠端 hexWeek6Demo的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f https://github.com/wendy60113/TransferUI.git master:gh-pages
+git push -f https://github.com/wendy60113/bootstrapvue-nesteddropdown.git master:gh-pages
 cd -
 
 # 執行指令，在終端機輸入 deploy.sh
